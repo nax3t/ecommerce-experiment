e51642c7016bff8cc61eee93fe8e5fa589dc972d
Zarko <propyl3n3@gmail.com>
2017-05-04 18:55:46 +0200

    add user login

--- config/passport.js
+++ config/passport.js
@@ -12,6 +12,7 @@ passport.deserializeUser(function (id, done) {
     });
 });
 
+// registration strategy
 passport.use("local.singup", new LocalStrategy({
     usernameField: "email",
     passwordField: "password",
@@ -42,6 +43,36 @@ passport.use("local.singup", new LocalStrategy({
                 return done(err);
             }
             return done(null, newUser);
-        })
+        });
+    });
+}));
+
+// sign in strategy
+passport.use("local.signin", new LocalStrategy({
+    usernameField: "email",
+    passwordField: "password",
+    passReqToCallback: true
+}, function (req, email, password, done) {
+    req.checkBody("email", " Invalid email").notEmpty().isEmail();
+    req.checkBody("password", "Invalid password").notEmpty();
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(function (error) {
+            messages.push(error.msg);
+        });
+        return done(null, false, req.flash("error", messages));
+    }
+    User.findOne({"email": email}, function (err, user) {
+        if (err) {
+            return done(err);
+        }
+        if (!user) {
+            return done(null, false, {message: "No user found."})
+        }
+        if (!user.validPassword(password)) {
+            return done(null, false, {message: "Incorrect password."})
+        }
+        return done(null, user);
     });
 }));
